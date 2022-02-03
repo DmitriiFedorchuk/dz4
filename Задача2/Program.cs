eeb185003f8b5c78a56c3d2965cf2a37ba6556b5
Dmitrii <fedorchukd@inbox.ru>
2022-02-03 13:00:06 +0300

    new

---
+++ Задача2/Program.cs
@@ -0,0 +1,17 @@
+﻿///Даны два числа. Показать большее и меньшее число
+Console.WriteLine("Введите первое число");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double b = double.Parse(Console.ReadLine());
+if (a>b){
+    Console.Write("Самое большое число =");
+    Console.WriteLine(a);
+    Console.Write("Самое маленькое число=");
+    Console.WriteLine(b);
+}
+else{
+    Console.Write("Самое большое число =");
+    Console.WriteLine(b);
+    Console.Write("Самое маленькое число=");
+    Console.WriteLine(a);
+}
